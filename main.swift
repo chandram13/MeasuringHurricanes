d44a9cf679e6843e7059a9e59b7a9a5e7734bffe
Marvish Subhas Chandra <mschandr@uci.edu>
2022-11-09 08:49:16 -0800

    Add files via upload

---
+++ main.swift
@@ -0,0 +1,17 @@
+// Marvish Chandra
+
+public class saffirSimpson{
+    func windScale(x){
+        if 95 > x > 74:
+        print("The given wind speed is determined to be a category one sized hurricane.")
+        if 110 > x > 96:
+        print("The given wind speed is determined to be a category two sized hurricane.")
+        if 129 > x > 11!:
+        print("The given wind speed is determined to be a category three sized hurricane.")
+        if 156 > x > 130:
+        print("The given wind speed is determined to be a category four sized hurricane.")
+        if 157 > x:
+        print("The given wind speed is determined to be a category four sized hurricane.")
+        else print("The given wind speed is either too less to be categorized as a hurricane or larger than a category five sized hurricane.")
+    }
+}
